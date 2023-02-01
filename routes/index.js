00b0a7dcaaa9f69a42369539f670821db65af0a5
Karen Yep <karenyeplee@gmail.com>
2023-02-01 16:37:16 -0600

    express-async-handler installed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+
+'use strict';
+
+const router = require('express').Router();
+const fsPromises = require('fs').promises;
+const path = require('path');
+const asyncHandler = require('express-async-handler');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async function (req, res) {
+  const filename = path.join(__dirname, '../README.md');
+  const readme = await fsPromises.readFile(filename, 'utf8');
+  res.render('index', { readme });
+}));
 
 module.exports = router;
